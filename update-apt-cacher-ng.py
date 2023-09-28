28088d53cd2b87e381820e7cb5c84bcf0bf122e5
AciDev <AciWebDev@gmail.com>
2023-09-28 14:22:19 +0100

    Updates

--- update-apt-cacher-ng.py
+++ update-apt-cacher-ng.py
@@ -1,31 +1,49 @@
+import time
 import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def main():
     parser = argparse.ArgumentParser(description="Update apt-cacher-ng")
     parser.add_argument("--url", required=True, help="URL of the proxy")
+    parser.add_argument("--browserless", required=True, help="URL of browserless")
 
     args = parser.parse_args()
 
     url_argument = args.url
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=400,350")
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.binary_location = "/usr/bin/chromium"
-
-    driver = webdriver.Chrome(options=chrome_options)
+    browserless_argument = args.browserless
+    
+    driver = create_con(browserless_argument)
     driver.get(f"{url_argument}/acng-report.html")
     click(driver, "doImport")
+    driver.quit()
+    
+    time.sleep(5)
+    
+    driver = create_con(browserless_argument)
     driver.get(f"{url_argument}/acng-report.html")
     click(driver, "doDownload")
     click(driver, "doMirror")
+    driver.quit()
+
+def create_con(browserless):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")  
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.binary_location = "/usr/bin/chromium"
+
+    driver = webdriver.Remote(
+        command_executor=f"{browserless}/webdriver",
+        options=chrome_options
+    )
+    return driver
 
 def click(driver, name):
-    elem = driver.find_element(By.NAME, name)
+    elem = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, name)))
     elem.click()
     return 0
 
